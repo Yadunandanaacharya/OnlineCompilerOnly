e9923f734d1de9a95d3c0f8988dfa0eea63d18ae
yadu <yadunandana@vegam.co>
2023-07-06 13:53:49 +0530

    Update index.js

---
+++ OnlineCompiler/BackEnd/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.json({online:"compiler"});
+});
+
+app.listen(5000, ()=>{
+    console.log("Server is running on port 5000");
+});
+
+//hellow world
+
